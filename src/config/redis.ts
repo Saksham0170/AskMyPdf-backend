98a978ee550333ef45d0d91d857740a10a4f0d6d
Saksham Garg <sakshamgarg782@gmail.com>
2026-01-11 14:02:50 +0530

    chore: run worker inside main server

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,4 +1,4 @@
-import { Redis } from "ioredis";
+import Redis from "ioredis";
 
 export const redisConfig = {
   username: process.env.REDIS_USERNAME ?? "default",
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import { app } from "./app";
 
+import { worker } from "./workers/file.worker";
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("Worker initialized:", !!worker);
 });
 
